0f598eee5a1a100a2056affa75cc6a2a4a96244e
dshepin <bright1308@gmail.com>
2020-10-05 23:04:29 +0300

    add

--- src/dshepin/myFirstFxApp/App.java
+++ src/dshepin/myFirstFxApp/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fx/window.fxml"));
-        primaryStage.setTitle("Коммунальный калькулятор");
+        primaryStage.setTitle("Коммунальный калькулятор.");
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
